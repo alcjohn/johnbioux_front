1affd207198d225bdae7bbb6e9f7d9b2187bbe5f
John Bioux <john.bioux@gmail.com>
2020-12-31 13:25:08 +0100

    test google font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="fr">
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap"
             rel="stylesheet"
