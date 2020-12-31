f1d852b6f86b08823525ea797bbc8692c3668571
John Bioux <john.bioux@gmail.com>
2020-12-31 13:33:02 +0100

    how optimise font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,11 @@ export default class Document extends NextDocument {
     return (
       <Html lang="fr">
         <Head>
-          {/* <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&display=swap"
             rel="stylesheet"
-          /> */}
+          />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
