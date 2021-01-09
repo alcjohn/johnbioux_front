c55ba58e767f0a43894ee70f30c0589455cdf19f
John Bioux <john.bioux@gmail.com>
2021-01-09 12:29:22 +0100

    dark

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ export default class Document extends NextDocument {
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
-          <ColorModeScript initialColorMode="system" />
+          <ColorModeScript initialColorMode="dark" />
           <Main />
           <NextScript />
         </body>
